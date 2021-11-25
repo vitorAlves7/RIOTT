d254c6ba364a7455b0fe215a817d2999297c053a
Vitor Alves <vitor.alves07750@outlook.com>
2021-11-25 16:20:01 -0300

    feat: Adicionado novos usuários e senhas
    Co-authored-by: Lívia Minucelli <liviawrk@gmail.com>

--- src/app/@core/services/auth.service.ts
+++ src/app/@core/services/auth.service.ts
@@ -11,7 +11,13 @@ export class AuthService {
   constructor(private router: Router) { }
 
   doLogin(user: User): void {
-    if (user.email === 'liviaminucelli@gmail.com' && user.password === '1234') {
+    if((user.email === 'liviaminucelli@gmail.com' && user.password === '1234') ||
+        (user.email === 'vitor.fer.alves7@gmail.com' && user.password === '5678') ||
+        (user.email === 'vitor.alves07750@outlook.com' && user.password === '9123') ||
+        (user.email === 'joao.firmino@tupper.com' && user.password === '4567') ||
+        (user.email === 'rosimaria.tavares@gmail.com' && user.password === '891') ||
+        (user.email === 'letícia.ferreira@gmail.com' && user.password === '234') ||
+        (user.email === 'gustavo.pereira@hotmail.com' && user.password === '5678') ) {
       this.userAuthenticated = true;
 
       this.router.navigate(['/pages/list']);
